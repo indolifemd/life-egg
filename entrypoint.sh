0fd79ea7153be6288e6c79a2389f363ed2c2d313
indolifemd <arkindolife@gmail.com>
2025-05-03 23:31:20 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
-# Cek jika git ada dan melakukan pull jika diatur untuk auto-update
-if [[ -d .git ]] && [[ ${AUTO_UPDATE} == "1" ]]; then
-  git pull
+cd /home/container
+
+# Ganti placeholder dengan nilai environment
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{[A-Z_][A-Z0-9_]*}}/${\1}/g')
+
+# Clone repo jika alamat GIT disediakan
+if [[ -n "${GIT_ADDRESS}" ]]; then
+    echo -e "[IndoLife] Cloning repo dari ${GIT_ADDRESS}"
+    
+    if [[ -d .git ]]; then
+        echo -e "[IndoLife] Repo sudah ada, skip clone"
+    else
+        if [[ -n "${USERNAME}" && -n "${ACCESS_TOKEN}" ]]; then
+            GIT_ADDRESS=$(echo "${GIT_ADDRESS}" | sed -e "s#https://#https://${USERNAME}:${ACCESS_TOKEN}@#")
+        fi
+
+        BRANCH="${BRANCH:-main}"
+        echo -e "[IndoLife] Checkout branch: ${BRANCH}"
+        git clone --single-branch --branch ${BRANCH} ${GIT_ADDRESS} .
+    fi
 fi
 
-# Install paket Node.js jika ada yang ditentukan
-if [[ ! -z ${NODE_PACKAGES} ]]; then
-  /usr/local/bin/npm install ${NODE_PACKAGES}
-fi
+# Jalankan perintah startup
+echo -e "[IndoLife] Menjalankan perintah startup:"
+echo -e "$MODIFIED_STARTUP"
 
-# Uninstall paket Node.js jika ada yang ditentukan
-if [[ ! -z ${UNNODE_PACKAGES} ]]; then
-  /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}
-fi
-
-# Install dependensi dari package.json jika ada
-if [ -f /home/container/package.json ]; then
-  /usr/local/bin/npm install
-fi
-
-# Perintah untuk menjalankan aplikasi
-/usr/local/bin/${CMD_RUN}
+eval "$MODIFIED_STARTUP"
